c5e11c008a42ec0811640e0eedcced17e9bdce71
fuu <fu@yihaofang.com>
2023-03-07 20:13:33 +0800

    Mock accounts update.

--- js/playground/src/config/accounts.js
+++ js/playground/src/config/accounts.js
@@ -2,29 +2,56 @@
 exports.Accounts = [
   {
     "encry": { 
-      "encoded": "f9xO6iqHlJ6LbMSi/XK6d9cKQPQVe8d2dkBcfKIJ1XsAgAAAAQAAAAgAAADKCD2JfUkv/xpglEBVWZttzfohn8QOxH6mukd8Ft89MpEmUj43Ieu2Sryvzy7D+BGPWaDf0lv8zhqdvJIKNAwMUDaOlii8b0soSvcYTHZ7rOFyzvHLv/waKy0797lluHcUJgqiidYSGF0mbAVWsS6fCWeNp24mtApuZFKqeo18zBHuyIkm56myxZscip3A7FHRzRdUZH4O6ZOPnS0B", 
+      "encoded":"f9xO6iqHlJ6LbMSi/XK6d9cKQPQVe8d2dkBcfKIJ1XsAgAAAAQAAAAgAAADKCD2JfUkv/xpglEBVWZttzfohn8QOxH6mukd8Ft89MpEmUj43Ieu2Sryvzy7D+BGPWaDf0lv8zhqdvJIKNAwMUDaOlii8b0soSvcYTHZ7rOFyzvHLv/waKy0797lluHcUJgqiidYSGF0mbAVWsS6fCWeNp24mtApuZFKqeo18zBHuyIkm56myxZscip3A7FHRzRdUZH4O6ZOPnS0B",
       "encoding": { "content": ["pkcs8", "sr25519"], "type": ["scrypt", "xsalsa20-poly1305"], "version": "3" }, 
-      "address": "5Dt3Diu9becXCqtY2nYucE7DYRaWb7a8V73xuphWeB7MbLVq",
-      "meta": { "name": "account_a"}
+      "address": "5DkpkKRiAqgrTt925fQ6jRZymhUFN52EpnskuHobaYKBqJ26",
+      "meta": { "name": "mock_a"}
     },
-    "password": "123456",
+    "password": "5Dkpk_123456",
   },
   {
     "encry": {
-      "encoded":"dAA8ngJm6ca9MWJqVuDojyaBxLk+3hapiTDxyD4+ZAMAgAAAAQAAAAgAAAC1XQ4a1Bzy2CLtgKOazPzjUhOUm8X5NEGaPS5vxIV3F0AvpMsuA8zPP9SiDWWr62RrjZCQ+qxOumQt56q50LO04gWO3xfhOozAur5N6jMLfHVG06kw15SzQg/wryREeVWyw/q8USeysTPch/6nACeTplVC6TBLUTJz5p9x8i/BdOJ1/DEz80ySJt1u33JZyWg/UA77AQDyhFFNxkCC",
+      "encoded":"oDkM3PGO2UAETUQZwNxdfn5XHOtppDjjFOMtCtWtkPsAgAAAAQAAAAgAAABFVI6EY8E+lNe8mpQpoRNailwluxtpsgKYrg3Fd3yHe48WTl1HkO2PdaJhM5u3JrJCojqXxeY8q0KwvY7jsZ6hE3ESbVEBhjO+fgh1jCjlvC4Nrj2yAySqc3J4+chXjP9g914B+MB2nNzQmwKpiLoVjEyIARxClNLmtpgaToHBb26XCf8O7wIreNRB6YJiK1gpL4c53raxvKtmZ1eG",
       "encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},
-      "address":"5DcpcBu1J4qpQRkeFy6Qcn9FxUm6knhvufnYpX62oHH1zWCx",
-      "meta":{"name":"account_b"}
+      "address":"5HYn3tEtVXrxmNDvyt5A8QzeJMMZ4R3coceBu7DcXgD6dRwp",
+      "meta":{"name":"mock_b"}
     },
-    "password": "123456",
+    "password": "5HYn3_123456",
   },
   {
     "encry": {
-      "encoded":"rAFoLywXyd+hSmv9SlTDOmkn746DymjuStyhRUmELB8AgAAAAQAAAAgAAAB5MQZyl8EoSyEsAymNrtzyFD8IV09C1TIbqyj+UcMmgiurOuJw17wi7gQiG8YFBD7e4gyrRRoQ0Aj5K7JUgL/QRGN5Gj9l9bWU9Vg/ZlIOTDmbkG/oOLUXV2tuo3ZyPpRQJdCQ7kdUPWFrsbCUyBEg0GBVAwsIB/LxILkRSCT2gLKdSoTWRYJgVDvrX08sEOPkQR1uNHARvIDFwQVQ",
+      "encoded":"j8RnW+feuMpuEFqzYt6MO5sCWGrdH0nR9b/14FfjQn0AgAAAAQAAAAgAAAAMYI3eq58T15mInD2vFr5eG/xrZCLo/ohddcaX7LKtp6i+y1JzueIQ0i+eAOCtRv2k8NKaHffVtaGBeFtBd3ol4ywji+WuKlY5C1FZ/izc6VkNr3bp+oMe3Recide2VD1j3SkVnSIds8wAgJCG7xF2YK7Bu6hUZCBuHj8mSSMGEbAATcAt221ELSowC780qGAzU5CjhantYPwO8C7P",
       "encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},
-      "address":"5G4NYSSkwk4hk13QYa5GvxogaeNnjA8qaXBqLk9BvxYXJvaW",
-      "meta":{"name":"account_c"}
+      "address":"5CUUuCwJbXo8jVJQW21huCFWtBrg2K61wbZBiyGP3V7ZC4Ko",
+      "meta":{"name":"mock_c"}
     },
-    "password": "123456",
+    "password": "5CUUu_123456",
+  },
+  {
+    "encry": {
+      "encoded":"r73XfdFk8xu9/lrA7jBJgH4K+KoA86rqc7Vv1W8chPkAgAAAAQAAAAgAAADgRe5kzEAxoJJiSJN60zmrC4HIEIr8MoYT8Zp+DSNRdmuWjShK4H/MOhDQuIcl/S08ZBA9euzZ8cX7qNbwiAckzbYCDdhyXMNe25FF84D/DTV2kjyRI4SDf1symYw95/OjYgbB1cRCkvmSN2rp3o4ZbEkQTilgOGWbXGm0YPJeDX8iP81TH9h9crsZ3Qs/JlTntf7NRov/sFlmqyKI",
+      "encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},
+      "address":"5GhU2JnMPGCNnoLbZmJBvrkGcVPm9AkzFUea4f99sHNa39jm",
+      "meta":{"name":"mock_d"}
+    },
+    "password": "5GhU2_123456",
+  },
+  {
+    "encry": {
+      "encoded":"nV6Xv+VIhSUEMDPwdqpqAsHAMAH48dT8ZBSZnLgHwksAgAAAAQAAAAgAAAB+GMrAztlDM0WFG/HhefK6o+qaWs291y5jwgz4EMmvpPkHsWzJVmVoTAlbfAocGjsE+lYX0hmNvudfJaB8OcQcgJW8WRrf3fik0pG11pq+58amwLCQBLmYQdI73DPJDIFSuLlF95toPsz9OPu0FoeyP5cp2rQ6UeaJd3pgT4nadI7lt+CYQD7evtfiBKXPV7CYvqP8aMbeVFK0Gnwh",
+      "encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},
+      "address":"5FHKmzFC3mDwG7pJKrBZqRWscV9E8hZDfer6aAG17VhQGuXB",
+      "meta":{"name":"account_e"}
+    },
+    "password": "5FHKm_123456",
+  },
+  {
+    "encry": {
+      "encoded":"2ixR6J+Sxp1HKBkqVOc1iDjkkqko1y8neP4XsEvuYOcAgAAAAQAAAAgAAABf7k+ZCxCldOZoQd5zLa0KdrLu/mzJe/ufaze3pBRln5NgPrls5M/T9RlFg0ge0n78/2cOwIhSUXA6RY0FJWYYHXTraV8xwnDNeOqkoDaDqAGRWT4EMHXy9ypzaJxjJqN2DGh4CJi9Ivt+dkorc2YFfq6TYGaDKGWjaMXjV1P+CU8L3K/1ZKKRu9xhGhTJlKyRc2At+WhUF4RgNfpH",
+      "encoding":{"content":["pkcs8","sr25519"],"type":["scrypt","xsalsa20-poly1305"],"version":"3"},
+      "address":"5CkLxxzqKs9uHhX8EWpJJRoCTcJD6ErTyEvad8nbVsTFS6gt",
+      "meta":{"name":"mock_f"}
+    },
+    "password": "5CkLx_123456",
   },
 ];
