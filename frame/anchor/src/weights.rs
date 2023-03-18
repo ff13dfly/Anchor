2cbd15053260201c4a2c3b4f230aea6f4cfc21c7
fuu <fu@yihaofang.com>
2023-03-18 19:50:29 +0800

    remove from_ref_time, try from_parts

--- frame/anchor/src/weights.rs
+++ frame/anchor/src/weights.rs
@@ -18,50 +18,45 @@ pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 
 	fn set_anchor(x: u32, ) -> Weight {
-		Weight::from_ref_time(0_u64)
+		Weight::from_parts(10_000, 12000)
 			.saturating_add(Weight::from_ref_time(1_000_000_u64).saturating_mul(x as u64))
 	}
 
 	fn set_sell() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 
 	fn set_unsell() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 
 	fn buy_anchor() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(T::DbWeight::get().writes(1_u64))
 	}
 }
 
 // For backwards compatibility and tests
 impl WeightInfo for () {
-	// fn set_anchor(x: u32, ) -> Weight {
-	// 	Weight::from_ref_time(0_u64)
-	// 		.saturating_add(Weight::from_ref_time(1_000_000_000_u64).saturating_mul(x as u64))
-	// }
-
 	fn set_anchor(x: u32, ) -> Weight {
-		Weight::from_parts(100, 1)
-			.saturating_add(Weight::from_parts(1_000_000_000, 1).saturating_mul(x as u64))
+		Weight::from_parts(10_000, 12000)
+			.saturating_add(Weight::from_ref_time(1_000_000_000_u64).saturating_mul(x as u64))
 	}
 
 	fn set_sell() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 
 	fn set_unsell() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 
 	fn buy_anchor() -> Weight {
-		Weight::from_ref_time(1_000_000_000_u64)
+		Weight::from_parts(10_000_000, 12000)
 			.saturating_add(RocksDbWeight::get().writes(1_u64))
 	}
 }
